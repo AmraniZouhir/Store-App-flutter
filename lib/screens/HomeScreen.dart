51ac5663df3ffea220d607abc5458f39f5623a39
zouhir amrani <zouhiramrani397@gmail.com>
2024-02-23 19:13:22 +0100

    setup fonders and them

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:storeapp/widgets/appBar_icons.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -15,6 +16,10 @@ class _HomeScreenState extends State<HomeScreen> {
         FocusScope.of(context).unfocus();
       },
       child: Scaffold(
+        appBar: AppBar(
+          title: Text('Home'),
+          leading: AppBarIcons(),
+        ),
         body: Center(
           child: Text('data'),
         ),
