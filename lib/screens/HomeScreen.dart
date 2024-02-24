f0e55f090c9b76b96412f288f3c2a3a46b53f432
zouhir amrani <zouhiramrani397@gmail.com>
2024-02-24 19:52:28 +0100

    Add prodact and page all product

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,7 +1,13 @@
+import 'package:card_swiper/card_swiper.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_iconly/flutter_iconly.dart';
+import 'package:page_transition/page_transition.dart';
 import 'package:storeapp/consts/global_colors.dart';
+import 'package:storeapp/screens/feeds_screen.dart';
 import 'package:storeapp/widgets/appBar_icons.dart';
+import 'package:storeapp/widgets/feeds_widget.dart';
 import 'package:storeapp/widgets/sale_widget.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -28,6 +34,8 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
     return GestureDetector(
       onTap: () {
         FocusScope.of(context).unfocus();
@@ -71,9 +79,66 @@ class _HomeScreenState extends State<HomeScreen> {
                     )),
               ),
               SizedBox(
-                height: 10,
+                height: 20,
               ),
-              SaleWidget(),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: size.height * 0.25,
+                        child: Swiper(
+                          itemCount: 3,
+                          itemBuilder: (context, index) {
+                            return SaleWidget();
+                          },
+                          pagination: const SwiperPagination(
+                              alignment: Alignment.bottomCenter,
+                              builder: DotSwiperPaginationBuilder(
+                                  color: Colors.white,
+                                  activeColor: Colors.red)),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.all(8),
+                        child: Row(
+                          children: [
+                            const Text(
+                              'Latest Products',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.w600, fontSize: 18),
+                            ),
+                            Spacer(),
+                            AppBarIcons(
+                                fonction: () {
+                                  Navigator.push(
+                                      context,
+                                      PageTransition(
+                                          type: PageTransitionType.fade,
+                                          child: FeedsScreen()));
+                                },
+                                icon: IconlyBold.arrowRight2)
+                          ],
+                        ),
+                      ),
+                      GridView.builder(
+                        shrinkWrap: true,
+                        physics: NeverScrollableScrollPhysics(),
+                        itemCount: 8,
+                        gridDelegate:
+                            const SliverGridDelegateWithFixedCrossAxisCount(
+                                crossAxisCount: 2,
+                                crossAxisSpacing: 0.0,
+                                mainAxisSpacing: 0.0,
+                                childAspectRatio: 0.6),
+                        itemBuilder: (context, index) {
+                          return const FeedsWidget();
+                        },
+                      )
+                    ],
+                  ),
+                ),
+              )
             ],
           ),
         ),
